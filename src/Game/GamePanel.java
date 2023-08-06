635f0ad2cb26549c5d464de1114d2f67d1079f91
Suicidal-Cat <darekolaric@gmail.com>
2023-08-06 17:44:19 +0200

    Promenjena sirina ekrana

--- src/Game/GamePanel.java
+++ src/Game/GamePanel.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 
 public class GamePanel extends JPanel implements Runnable {
 
-	static final int GAME_WIDTH = 1000;
+	static final int GAME_WIDTH = 1100;
 	static final int GAME_HEIGHT = (int) (GAME_WIDTH * (0.5555));
 	static final Dimension SCREEN_SIZE = new Dimension(GAME_WIDTH, GAME_HEIGHT);
 	static final int BALL_DIAMETER = 20;
@@ -41,8 +41,8 @@ public class GamePanel extends JPanel implements Runnable {
 	}
 
 	public void newPaddles() {
-		paddle1 = new Paddle(0, (GAME_HEIGHT / 2) - (PADDLE_HEIGHT / 2), PADDLE_WIDTH, PADDLE_HEIGHT, 1);
-		paddle2 = new Paddle(GAME_WIDTH - PADDLE_WIDTH, (GAME_HEIGHT / 2) - (PADDLE_HEIGHT / 2), PADDLE_WIDTH,
+		paddle1 = new Paddle(10, (GAME_HEIGHT / 2) - (PADDLE_HEIGHT / 2), PADDLE_WIDTH, PADDLE_HEIGHT, 1);
+		paddle2 = new Paddle(GAME_WIDTH - PADDLE_WIDTH-10, (GAME_HEIGHT / 2) - (PADDLE_HEIGHT / 2), PADDLE_WIDTH,
 				PADDLE_HEIGHT, 2);
 
 	}
